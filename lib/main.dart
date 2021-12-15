fe3f71eb42e005c748b0cc55e20e87ca4a637bd6
zain_ali <zain_ali@gmail.com>
2021-12-15 12:19:05 +0500

    dev2 changes

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,26 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Welcome App')),
-      body: Container(
-        child: Center(child: Text('Hii Baby')),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          alignment: Alignment.center,
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+              color: Colors.teal,
+              shape: BoxShape.circle,
+              //borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey, blurRadius: 5, offset: Offset(2.0, 5.0))
+              ],
+              gradient: LinearGradient(colors: [Colors.green, Colors.pink])),
+          child: Text(
+            'Hi Baby',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ),
       ),
     );
     // return Container(
