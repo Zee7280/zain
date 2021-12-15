56de30b16fa7bbe00da89c3b1ef05e0500d68015
zain_ali <zain_ali@gmail.com>
2021-12-15 13:00:06 +0500

    chap 2 complete

--- lib/main.dart
+++ lib/main.dart
@@ -15,27 +15,68 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Welcome App')),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
         child: Container(
-          padding: const EdgeInsets.all(20),
-          alignment: Alignment.center,
-          width: 100,
-          height: 100,
-          decoration: BoxDecoration(
-              color: Colors.teal,
-              shape: BoxShape.circle,
-              //borderRadius: BorderRadius.circular(10),
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.grey, blurRadius: 5, offset: Offset(2.0, 5.0))
-              ],
-              gradient: LinearGradient(colors: [Colors.green, Colors.pink])),
-          child: Text(
-            'Hi Baby',
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          color: Colors.black45,
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(20),
+                alignment: Alignment.center,
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                    color: Colors.teal,
+                    // shape: BoxShape.circle,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 5,
+                          offset: Offset(2.0, 5.0))
+                    ],
+                    gradient:
+                        LinearGradient(colors: [Colors.green, Colors.pink])),
+                child: Text(
+                  'Hi Baby',
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(20),
+                alignment: Alignment.center,
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                    color: Colors.teal,
+                    shape: BoxShape.circle,
+                    //borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 5,
+                          offset: Offset(2.0, 5.0))
+                    ],
+                    gradient:
+                        LinearGradient(colors: [Colors.green, Colors.pink])),
+                child: Text(
+                  'yahooo ',
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+            //)
           ),
         ),
       ),
+      //Center( child:
     );
     // return Container(
     //   color: Colors.green,
